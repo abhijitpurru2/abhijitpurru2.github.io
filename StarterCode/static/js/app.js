1e286e5dea19c2c6a38dfa214a8192ab66aa14a0
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-28 16:54:09 -0500

    Add files via upload

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,53 @@
+function dropdownCreator() {
+    var dropdownMenu = d3.select("#selDataset");
+    d3.json('samples.json').then((sampleData) => {
+        var dataNames = sampleData.names;
+        dataNames.forEach((info) => {
+            dropdownMenu.append("option")
+                .text(info)
+                .property("value", info)
+        });
+        metadataVisual(dataNames[0]);
+        chartsVisual(dataNames[0]);
+    });
+}
+
+function optionChanged(sampleNo) {
+    metadataVisual(sampleNo);
+    chartsVisual(sampleNo);
+}
+
+function chartsVisual(sampleNo){
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var info = samples.filter(sampleObject => sampleObject.id == sampleNo);
+
+        var otu_ids = info[0].otu_ids;
+        var otu_labels = info[0].otu_labels;
+        var sample_values = info[0].sample_values;
+
+        var barChart = [{
+            x: sample_values.slice(0, 10).reverse(),
+            y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: 'bar',
+            orientation: 'h'
+        }];
+
+        Plotly.newPlot('bar', barChart);
+    });
+}
+
+function metadataVisual(sampleNo){
+    d3.json("samples.json").then((data) => {
+        var metadataD3 = d3.select("#sample-metadata");
+        metadataD3.html("");
+        var metadataInfo = data.metadata;
+        var info = metadataInfo.filter(sampleObject => sampleObject.id == sampleNo);
+        Object.entries(info[0]).forEach(([key, value]) => {
+            metadataD3.append("h6").text(`${key}: ${value}`);
+        });
+    });
+}
+
+dropdownCreator();
